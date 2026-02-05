2fd61ea4bc2a311bf39b76fe52a839567078e299
vj <venkatesh.jatla@gmail.com>
2026-02-04 20:31:57 -0500

    Published website on 2026-02-04 at 20:31:56

--- publish.el
+++ publish.el
@@ -4,6 +4,27 @@
 (require 'org)
 (require 'ox-publish)
 
+;; Function to convert denote links to file links during export (non-destructive)
+(defun convert-denote-links-in-buffer (backend)
+  "Convert denote: links to file: links during export without modifying source files."
+  (goto-char (point-min))
+  ;; Find all denote links: [[denote:IDENTIFIER][Description]]
+  (while (re-search-forward "\\[\\[denote:\\([0-9T]+\\)\\]\\[\\([^]]+\\)\\]\\]" nil t)
+    (let* ((identifier (match-string 1))
+           (description (match-string 2))
+           (base-dir (or (and (buffer-file-name)
+                              (file-name-directory (buffer-file-name)))
+                         default-directory))
+           ;; Find the actual file with this identifier
+           (files (directory-files base-dir nil (concat "^" identifier "--.*\\.org$"))))
+      (when files
+        (let ((target-file (car files)))
+          ;; Replace denote link with file link in the buffer (temporary)
+          (replace-match (format "[[file:%s][%s]]" target-file description) t t))))))
+
+;; Add the conversion function to org export hook
+(add-hook 'org-export-before-parsing-hook 'convert-denote-links-in-buffer)
+
 ;; Define the org-publish project alist
 (setq org-publish-project-alist
       '(("org-notes"
